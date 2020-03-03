50f4f24df1005dd9c1b8e445e6ffa37a6bbeb3a8
QUANG TRUC ĐINH <quangtruc111@gmail.com>
2020-03-03 11:07:48 +0700

    update In/Out history

---
+++ Bottom-SPA/src/app/_core/_models/history.ts
@@ -0,0 +1,10 @@
+export class History {
+    date: string;
+    status: string;
+    plan_no: string;
+    inOut_no: string;
+    batch: number;
+    mat_name: string;
+    source_location: string;
+    destination_location: string;
+}
