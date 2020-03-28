de348d12d80226e9bbf15da3e88247adea1cfcab
Long Nguyen Phi <philong26792@outlook.com.vn>
2020-03-28 15:27:58 +0700

    #fix page print qrcode

--- Bottom-SPA/src/app/views/qr-generate/qr-print/qr-print.component.ts
+++ Bottom-SPA/src/app/views/qr-generate/qr-print/qr-print.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { QRCodeMainModel } from '../../../_core/_viewmodels/qrcode-main-model';
+import { PackingListDetailModel } from '../../../_core/_viewmodels/packing-list-detail-model';
 
 @Component({
   selector: 'app-qr-print',
@@ -7,29 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrPrintComponent implements OnInit {
   elementType: 'url' | 'canvas' | 'img' = 'url';
-  value: string = '0123456789';
-  TSizeArray: any[];
-  sizeArray:  any[];
-  PQty:  any[];
-  RQty:  any[];
-  Bal:  any[];
+  @Input() qrCodeMainItem: QRCodeMainModel;
+  @Input() packingListDetail: PackingListDetailModel[] = [];
+  @Input() totalQty: number;
   constructor() { }
 
   ngOnInit() {
-    this.TSizeArray = ['03.5', '03.5', '03.5', '03.5', '03.5', '03.5', '03.5', '03.5'];
-    this.sizeArray = ['03.5', '04', '04.5', '05', '05.5', '06', '06', '06'];
-    this.PQty = [300, 200, 300, 100, 100, 50, 50, 50];
-    this.RQty = [300, 200, 300, 100, 100, 50, 50, 50];
-    this.Bal = [0, 0, 0, 0, 0, 0, 0, 0];
+
   }
-  // generateQRCode() {
-  //   if (this.qrcodename === '') {
-  //     this.display = false;
-  //     alert ('Please enter the name');
-  //     return;
-  //   } else {
-  //     this.value = this.qrcodename;
-  //     this.display = true;
-  //   }
-  // }
+
 }
